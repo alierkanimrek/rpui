d723d9218a9aa21e079449b53a57c23371548b61
alierkanimrek <alierkanimrek@gmail.com>
2020-03-16 10:38:38 +0300

    Licence changes

--- src/ui_handlers/front/routing.py
+++ src/ui_handlers/front/routing.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-''' All rights reserved (c) 2018 Ali Erkan IMREK <alierkanimrek@gmail.com> '''
+'''
+    This file is part of Rplexus Server Application.
+
+    Rplexus Server Application is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Rplexus Server Application is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rplexus Server Application.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+    Copyright 2019,2020 Ali Erkan IMREK <alierkanimrek@gmail.com>
+'''
+
+
+
+
 
 
 
